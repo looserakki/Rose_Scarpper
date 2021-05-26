fe01920e769b45178cca1044a28280a7b3d62cc3
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-26 21:38:11 +0530

    Update and rename Start.py to Scrape.py

---
+++ RSc/modules/Scrape.py
@@ -0,0 +1,22 @@
+from RSc import ubot, OWNER_ID
+from telethon import events
+from functions.messages import ImportChatInviteRequest
+
+@tbot.on(events.NewMessage(pattern="^/start$"))
+async def event(event):
+ await event.reply("RSc Scrapper is online With 5/5 Clients Active.")
+
+@tbot.on(events.NewMessage(pattern="^/scrape ?(.*)"))
+async def sc(event):
+ if not event.sender_id == OWNER_ID:
+    return
+ if not event.pattern_match.group(1):
+    return await event.reply("Please enter the chat username to start scrapping members.")
+ chat_username = event.pattern_match.group(1)
+ try:
+   chat = await tbot.get_entity(chat_username)
+   username = chat.username
+ except:
+   return await event.reply("❌Invalid chat provided.")
+ await ubot(ImportChatInviteRequest(hash=username))
+ 
