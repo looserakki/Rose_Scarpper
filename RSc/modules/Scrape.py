2fd37b5f8b6120c4baa1bdbacac70df0ae1ea601
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-26 22:06:59 +0530

    Update Scrape.py

--- RSc/modules/Scrape.py
+++ RSc/modules/Scrape.py
@@ -1,10 +1,13 @@
 from RSc import ubot, OWNER_ID
 from telethon import events
+import io
 from functions.messages import ImportChatInviteRequest
 
 @tbot.on(events.NewMessage(pattern="^/start$"))
 async def event(event):
  await event.reply("RSc Scrapper is online With 1/15 Clients Active.")
+
+members = []
 
 @tbot.on(events.NewMessage(pattern="^/scrape ?(.*)"))
 async def sc(event):
@@ -19,4 +22,22 @@ async def sc(event):
  except:
    return await event.reply("❌Invalid chat provided.")
  await ubot(ImportChatInviteRequest(hash=username))
- 
+ async for user in ubot.iter_participants(username):
+   if not user.bot:
+     if user.username:
+       members.append(user.username)
+
+@tbot.on(events.NewMessage(pattern="^/members$")
+async def mem(event):
+ if len(members) == 0:
+   return await event.reply("Scrape some members first")
+ with io.BytesIO(str.encode(str(members))) as out_file:
+    out_file.name = "members.txt"
+    await tbot.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption=str(len(members)),
+                reply_to=reply_to_id,
+            )
