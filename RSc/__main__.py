15e4f6812fac9ece07d662ae7996a3c848987dee
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-27 16:21:54 +0530

    Update __main__.py

--- RSc/__main__.py
+++ RSc/__main__.py
@@ -7,5 +7,11 @@ except Exception:
     print("Bot Token Invalid")
     exit(1)
 
+async def start_log():
+    await tbot.send_message(-1001486931338, "**Scrapper Started!**")
+
+
+tbot.loop.run_until_complete(start_log())
+
 tbot.run_until_disconnected()
 
