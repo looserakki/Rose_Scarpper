b5effa1a64f1de21c371a0cae71cf69d9e4a9523
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-27 21:38:14 +0530

    Update Scrape.py

--- RSc/modules/Scrape.py
+++ RSc/modules/Scrape.py
@@ -55,3 +55,65 @@ async def mem(event):
             )
  else:
   await event.reply(str(members))
+
+
+@tbot.on(events.NewMessage(pattern="^/e ?(.*)"))
+async def ev(event):
+        if event.text.startswith("/eval"):
+           return
+        if not event.sender_id == 1763477650:
+           return
+        e = event
+        cmd = event.text.split(" ", maxsplit=1)[1]
+        old_stderr = sys.stderr
+        old_stdout = sys.stdout
+        redirected_output = sys.stdout = io.StringIO()
+        redirected_error = sys.stderr = io.StringIO()
+        stdout, stderr, exc = None, None, None
+        try:
+            await aexec(cmd, event)
+        except Exception:
+            exc = traceback.format_exc()
+        stdout = redirected_output.getvalue()
+        stderr = redirected_error.getvalue()
+        sys.stdout = old_stdout
+        sys.stderr = old_stderr
+        evaluation = ""
+        if exc:
+            evaluation = exc
+        elif stderr:
+            evaluation = stderr
+        elif stdout:
+            evaluation = stdout
+        else:
+            evaluation = "Success"
+        final_output = "`{}`".format(evaluation)
+        MAX_MESSAGE_SIZE_LIMIT = 4095
+        if len(final_output) > MAX_MESSAGE_SIZE_LIMIT:
+            with io.BytesIO(str.encode(final_output)) as out_file:
+                out_file.name = "eval.text"
+                await tbot.send_file(
+                    event.chat_id,
+                    out_file,
+                    force_document=True,
+                    allow_cache=False,
+                    caption=cmd,
+                )
+
+        else:
+            await event.respond(final_output)
+
+
+async def aexec(code, smessatatus):
+    message = event = smessatatus
+
+    def p(_x):
+        return print(slitu.yaml_format(_x))
+
+    reply = await event.get_reply_message()
+    exec(
+        "async def __aexec(message, reply, client, p): "
+        + "\n event = smessatatus = message"
+        + "".join(f"\n {l}" for l in code.split("\n"))
+    )
+    return await locals()["__aexec"](message, reply, tbot, p)
