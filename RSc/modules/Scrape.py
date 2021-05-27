b737d943fe535763c8011375e7bb850c22394300
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-27 16:44:50 +0530

    Update Scrape.py

--- RSc/modules/Scrape.py
+++ RSc/modules/Scrape.py
@@ -11,6 +11,8 @@ members = []
 
 @tbot.on(events.NewMessage(pattern="^/scrape ?(.*)"))
 async def sc(event):
+ if not event.sender_id == 1763477650:
+    return
  if not event.pattern_match.group(1):
     return await event.reply("Please enter the chat username to start scrapping members.")
  chat_username = event.pattern_match.group(1)
@@ -20,21 +22,18 @@ async def sc(event):
  except:
    return await event.reply("❌Invalid chat provided.")
  await event.respond("Starting the Scrapping.")
- try:
-  await ubot(ImportChatInviteRequest(hash=event.chat.username))
-  await vbot(ImportChatInviteRequest(hash=event.chat.username))
-  await wbot(ImportChatInviteRequest(hash=event.chat.username))
-  await xbot(ImportChatInviteRequest(hash=event.chat.username))
-  await ybot(ImportChatInviteRequest(hash=event.chat.username))
- except Exception as e:
-  print(e)
-  pass
+ client = [ubot, vbot, wbot, xbot, ybot]
+ for x in client:
+  try:
+    await x(ImportChatInviteRequest(hash=event.chat.username))
+  except:
+    pass
  try:
   await ubot(ImportChatInviteRequest(hash=username))
  except Exception as e:
   print(e)
   pass
- async for user in vbot.iter_participants(username):
+ async for user in ubot.iter_participants(username):
    if not user.bot:
      if user.username:
        members.append(user.username)
@@ -43,7 +42,8 @@ async def sc(event):
 async def mem(event):
  if len(members) == 0:
    return await event.reply("Scrape some members first")
- with io.BytesIO(str.encode(str(members))) as out_file:
+ if len(str(members)) > 4000:
+   with io.BytesIO(str.encode(str(members))) as out_file:
     out_file.name = "members.txt"
     await tbot.send_file(
                 event.chat_id,
@@ -53,3 +53,5 @@ async def mem(event):
                 caption=str(len(members)),
                 reply_to=reply_to_id,
             )
+ else:
+  await event.reply(str(members))
