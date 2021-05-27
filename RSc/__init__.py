892d2cd326d53f297efa82b74a8b290434c38ad1
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-27 15:50:49 +0530

    Update __init__.py

--- RSc/__init__.py
+++ RSc/__init__.py
@@ -19,15 +19,25 @@ H_2 = os.environ.get("HASH_2")
 H_3 = os.environ.get("HASH_3")
 H_4 = os.environ.get("HASH_4")
 H_5 = os.environ.get("HASH_5")
+
 OWNER_ID = os.environ.get("OWNER_ID")
 
 TOKEN = os.environ.get("TOKEN")
 tbot = TelegramClient(None, K_1, H_1)
 
 ubot = TelegramClient(StringSession(S_1), K_1, H_1)
+vbot = TelegramClient(StringSession(S_2), K_2, H_2)
+wbot = TelegramClient(StringSession(S_3), K_3, H_3)
+xbot = TelegramClient(StringSession(S_4), K_4, H_4)
+ybot = TelegramClient(StringSession(S_5), K_5, H_5)
+
 
 try:
   ubot.start()
+  vbot.start()
+  wbot.start()
+  xbot.start()
+  ybot.start()
 except Exception as e:
   print(e)
   sys.exit()
